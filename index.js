7125089c4cce69ccf38a3f2e12e4692564e6c612
JeanMieses <araujo0275@gmail.com>
2022-11-14 16:01:19 -0500

    Adding remove functionality and updating CSS

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const completeBtn = document.querySelector(".completeBtn");
 const allBtn = document.querySelector(".allBtn");
 const activeBtn = document.querySelector(".activeBtn");
 const todosLeftText = document.querySelector(".left");
+const removeElement = document.querySelector(".remove");
 
 let draggingElementIndex = null;
 let dataIndex = lis.length;
@@ -17,22 +18,18 @@ userInput.addEventListener("submit", (e) => {
   e.preventDefault();
   let input = e.target[0].value;
 
-  if (!input) {
-    alert("You need to typea todo!");
-  } else {
-    createElement(input);
-  }
-
+  if (!input) alert("You need to typea todo!");
+  createElement(input);
   e.target[0].value = "";
   todosLeft++;
   todosLeftText.innerText = todosLeft;
-  
 });
 
 //event listeners for drag events
 container.addEventListener("dragstart", dragStart);
 container.addEventListener("drop", dragDrop);
 container.addEventListener("dragover", dragOver);
+container.addEventListener("click", removeElementF);
 
 //functions that execute for dragging events
 function dragStart(e) {
@@ -52,6 +49,10 @@ function dragOver(e) {
   e.preventDefault();
 }
 
+function dragEnter(e) {
+  e.preventDefault();
+}
+
 //switch element when drapping over another
 function switchElements(from, to) {
   let itemOne = container.children[from].querySelector(".draggable");
@@ -66,15 +67,26 @@ function createElement(userInput) {
   const li = document.createElement("li");
   const p = document.createElement("p");
   const input = document.createElement("input");
+  const button = document.createElement("x");
 
   input.name = "todos";
   input.type = "radio";
   p.draggable = true;
   p.classList.add("draggable");
   li.setAttribute("data-index", dataIndex++);
-
+  button.classList.add("remove");
+  button.append("X");
   p.append(userInput);
   li.append(input);
   li.append(p);
+  li.append(button);
   container.append(li);
+}
+
+function removeElementF(e) {
+  if (e.target.classList.contains("remove")) {
+    e.target.parentNode.remove();
+    todosLeft--;
+    todosLeftText.innerText = todosLeft;
+  }
 }
