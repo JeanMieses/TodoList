9b04586c49bc83ad73485f0420d20a32dd3d470c
JeanMieses <araujo0275@gmail.com>
2022-11-16 23:21:07 -0500

    Adding toggle between todoss functionality and CSS adjustmnets

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const draggables = document.querySelectorAll(".draggable");
 const lis = document.querySelectorAll("li");
 const container = document.querySelector("#container");
 const userInput = document.querySelector("#userInput");
-const completeBtn = document.querySelector(".completeBtn");
-const allBtn = document.querySelector(".allBtn");
-const activeBtn = document.querySelector(".activeBtn");
 const todosLeftText = document.querySelector(".left");
 const removeElement = document.querySelector(".remove");
+const completeBtn = document.querySelector(".completeBtn");
+const activeBtn = document.querySelector(".activeBtn");
+const allBtn = document.querySelector(".allBtn");
 
 let draggingElementIndex = null;
 let dataIndex = lis.length;
@@ -33,6 +33,10 @@ container.addEventListener("dragstart", dragStart);
 container.addEventListener("drop", dragDrop);
 container.addEventListener("dragover", dragOver);
 container.addEventListener("click", removeElementF);
+container.addEventListener("click", markComplete);
+completeBtn.addEventListener("click", showCompletedTodos);
+activeBtn.addEventListener("click", activeTodos);
+allBtn.addEventListener("click", showAllTodos);
 
 //functions that execute for dragging events
 function dragStart(e) {
@@ -65,15 +69,66 @@ function switchElements(from, to) {
   container.children[to].append(itemOne);
 }
 
+function removeElementF(e) {
+  if (e.target.classList.contains("remove")) {
+    e.target.parentNode.parentNode.remove();
+    todosLeft--;
+    todosLeftText.innerText = todosLeft;
+  }
+}
+
+function markComplete(e) {
+  if (e.target.nodeName === "DIV") {
+    e.target.classList.toggle("complete");
+    e.target.parentNode.classList.toggle("done");
+  }
+}
+
+function showCompletedTodos() {
+  this.style.color='hsl(220, 98%, 61%)';
+  activeBtn.style.color='hsl(234, 11%, 52%)';
+  allBtn.style.color='hsl(234, 11%, 52%)';
+  for (let li of container.children) {
+    if (!li.classList.contains("done")) {
+      li.classList.add("hide");
+    }
+    if (li.classList.contains("done")) {
+      li.classList.remove("hide");
+    }
+  }
+}
+
+function activeTodos() {
+  this.style.color='hsl(220, 98%, 61%)';
+  completeBtn.style.color='hsl(234, 11%, 52%)';
+  allBtn.style.color='hsl(234, 11%, 52%)';
+  for (let li of container.children) {
+    if (li.classList.contains("done")) {
+      li.classList.add("hide");
+    }
+
+    if (!li.classList.contains("done")) {
+      li.classList.remove("hide");
+    }
+  }
+}
+
+function showAllTodos() {
+  this.style.color='hsl(220, 98%, 61%)';
+  activeBtn.style.color='hsl(234, 11%, 52%)';
+  completeBtn.style.color='hsl(234, 11%, 52%)';
+  for (let li of container.children) {
+    li.classList.remove("hide");
+  }
+}
+
 //create a DOM element
 function createElement(userInput) {
   const li = document.createElement("li");
   const p = document.createElement("p");
-  const input = document.createElement("input");
-  const button = document.createElement("x");
-
-  input.name = "todos";
-  input.type = "radio";
+  const div = document.createElement("div");
+  const button = document.createElement("button");
+  div.classList.add("check");
   p.draggable = true;
   p.classList.add("draggable");
   li.setAttribute("data-index", dataIndex++);
@@ -81,15 +136,7 @@ function createElement(userInput) {
   button.append("x");
   p.append(userInput);
   p.append(button);
-  li.append(input);
+  li.append(div);
   li.append(p);
   container.append(li);
-}
-
-function removeElementF(e) {
-  if (e.target.classList.contains("remove")) {
-    e.target.parentNode.parentNode.remove();
-    todosLeft--;
-    todosLeftText.innerText = todosLeft;
-  }
 }
